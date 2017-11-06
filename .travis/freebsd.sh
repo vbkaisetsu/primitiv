6e17d2b4ffd3c690f4d6b2e99527539bcd568386
Koichi Akabe <vbkaisetsu@gmail.com>
2017-11-06 18:11:53 +0900

    Fix

--- .travis/freebsd.sh
+++ .travis/freebsd.sh
@@ -5,14 +5,12 @@ set -xe
 sudo apt install -y qemu sshpass xz-utils
 wget https://github.com/vbkaisetsu/qemu-freebsd-ssh-enabled/raw/master/FreeBSD-11.1-RELEASE-amd64.qcow2.xz
 xz -d ./FreeBSD-11.1-RELEASE-amd64.qcow2.xz
-sudo qemu-system-x86_64 ./FreeBSD-11.1-RELEASE-amd64.qcow2 -m 1600 -net user,hostfwd=tcp::10022-:22 -net nic -nographic &
+sudo qemu-system-x86_64 ./FreeBSD-11.1-RELEASE-amd64.qcow2 -m 2048 -net user,hostfwd=tcp::10022-:22 -net nic -nographic &
 
-sleep 60
+sleep 90
 
 export SSHCMD="sshpass -p password ssh -o ServerAliveInterval=30 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@localhost -p10022"
 export SCPCMD="sshpass -p password scp -o ServerAliveInterval=30 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P10022 -r"
-
-$SCPCMD $TRAVIS_BUILD_DIR root@localhost:primitiv
 
 # install
 $SSHCMD "env ASSUME_ALWAYS_YES=YES pkg update"
@@ -21,6 +19,8 @@ $SSHCMD "python3 -m ensurepip"
 $SSHCMD "pip3 install numpy cython"
 
 # script
+$SCPCMD $TRAVIS_BUILD_DIR root@localhost:primitiv
+
 $SSHCMD "cd primitiv && cmake . -DPRIMITIV_BUILD_TESTS=ON -DCMAKE_INSTALL_PREFIX=/usr"
 $SSHCMD "cd primitiv && make VERBOSE=1"
 $SSHCMD "cd primitiv && make test"
